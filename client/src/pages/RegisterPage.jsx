25592dba0c46e148fef90b8afd08001b6eca0c9c
Rahul <rahul98kumarpvt@gmail.com>
2024-09-06 16:33:52 +0530

    first commit

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(e) {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/register", {
+      method: "Post",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status === 200) {
+      alert("Registration Successful");
+    } else {
+      alert("Registration Failed");
+    }
+  }
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
